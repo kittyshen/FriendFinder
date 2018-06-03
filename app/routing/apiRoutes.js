40e2ceaa6e73631f0e6fac940a2a76b4c0089436
Kitty <kittykumas@gmail.com>
2018-06-03 15:38:44 -0700

    adjusted the user input to check whether user entered the right link for their profile pic, if not valid link use a default unknow person img; thanks for alvin find the jquery always promise call usuage to make this possible; adjusted the style for the survey question part; commented our the filereading while filewriting in apiRoute.js otherwise it will sometimes crash the server due to trying to read same file while writing haven't finished yet; async logic is super hard to deal with >_<

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -38,7 +38,29 @@ module.exports = function(app){
 
         var json = req.body;
         // console.log(json);
-        // var person = { name : json.name, photo : json.photo };
+
+        
+        //define a fucntion to compare the score property of one obj from the obj array to target obj
+        function compare(obj,targetObj){
+            var totalDiff = 0;  //capture the difference of each score inside the score array
+            for(var i = 0; i<10; i++){
+                totalDiff += Math.abs(parseInt(obj.score[i]) - parseInt(targetObj.score[i]));
+            }
+            return totalDiff;
+        }
+        // get old user infomation, which will be a list of obj, and do the comparition
+        var minDiff = 40; // start with max possible difference points
+        var minIndex = 0;
+        for(var i = 0; i<user.length; i++){
+            var currentDiff = compare(user[i],json);  // get the total current difference of the old user obj in the user array to just added new target user
+
+            if(currentDiff < minDiff){
+                minDiff = currentDiff; 
+                minIndex = i;   //capture the minDifference user's index id to be used later as return object
+            }
+        }
+        var returnMatch = user[minIndex];
+
 
         console.log(user);
         user.push(json);
@@ -47,20 +69,22 @@ module.exports = function(app){
             if (err) {
                 return console.log(err);
             }
+            
         });
 
-        fs.readFile(path.join(__dirname, "../data/friends.txt"),"utf8" , function(err,data){
-            if (err) {
-                return console.log(err);
-            }
-            user = JSON.parse(data);
-            console.log(user);
+        // fs.readFile(path.join(__dirname, "../data/friends.txt"),"utf8" , function(err,data){
+        //     if (err) {
+        //         return console.log(err);
+        //     }
+        //     user = JSON.parse(data);
+        //     // console.log(user);
 
-        });
+        // });
 
         console.log(json);
-        users.push(json);
-        res.json(json); 
+        // users.push(json);
+        // res.json(json); 
+        res.json(returnMatch); 
 
     });
 }
